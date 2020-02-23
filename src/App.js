8dd2c111bd52f5b20d74845c9a261619fc9b6f49
ridoansaleh <ridoansalehnasution98@gmail.com>
2020-02-23 10:07:20 +0700

    use submodule's component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./child-repo/src/components/Header";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Header />
     </div>
   );
 }
